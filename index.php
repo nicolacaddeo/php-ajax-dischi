534c8e749f3f3a48c1344ce93f0e56eec927918b
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-10-17 21:13:25 +0200

    boostrap removed

--- index.php
+++ index.php
@@ -12,8 +12,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.1.3/axios.min.js" integrity="sha512-0qU9M9jfqPw6FKkPafM3gy2CBAvUWnYVOfNPDYKVuRTel1PrciTj+a9P3loJB+j0QmN2Y0JYQmkBBS8W+mbezg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- My script -->
     <script type="text/javascript" src="js/script.js" defer></script>
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -23,7 +21,9 @@
 </head>
 <body>
     <div id="root">
-        
+        <div class="container">
+
+        </div>
     </div>
 </body>
 </html>
